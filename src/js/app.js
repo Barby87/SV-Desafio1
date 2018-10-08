0f5ebe8f139dc4d9d9638df6666edc6bc86d7f6e
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-10-08 11:57:30 -0300

    First commit Agregar comentario y eliminar funcionando

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+
+const btn = document.getElementById('btn');
+function comment() {
+    let comments = document.getElementById('addComment').value;
+    document.getElementById('addComment').value = '';
+    const containerComment = document.getElementById('containerComment');
+    const newComments = document.createElement('div');
+
+    //validar que el textarea tenga un comentario
+    if (comments.length === 0 || comments === null) {
+        alert('Debes ingresar un mensaje');
+        return false;
+    }
+
+    const trash = document.createElement('i');
+    trash.classList.add('fa', 'fa-trash', 'trash');
+    const separator = document.createElement('hr');
+
+    const contenedorElemento = document.createElement('p');
+    let textNewComment = document.createTextNode(comments);
+    contenedorElemento.appendChild(textNewComment);
+    newComments.appendChild(trash);
+    newComments.appendChild(contenedorElemento);
+    containerComment.appendChild(newComments);
+    newComments.appendChild(separator);
+
+
+    trash.addEventListener('click', () => {
+        containerComment.removeChild(newComments);
+    })
+
+}
+
+
+
+
+
